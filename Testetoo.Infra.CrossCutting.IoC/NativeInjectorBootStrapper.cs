b91d284e74cdd41b5e5c82899fb4a8ea5ed55ece
Daniel Gomes <programad@gmail.com>
2018-12-25 15:09:34 -0200

    ajuste de referencia

--- Testetoo.Infra.CrossCutting.IoC/NativeInjectorBootStrapper.cs
+++ Testetoo.Infra.CrossCutting.IoC/NativeInjectorBootStrapper.cs
@@ -1,6 +1,4 @@
-﻿
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Testetoo.Application.Interfaces;
 using Testetoo.Application.Services;
 using Testetoo.Domain.Interfaces;
